44beb9a5985a165da455b33c35d9c1a926c58da5
Santiago Fernando Gomez <sfgomez1998@gmail.com>
2026-05-10 00:05:04 +0200

    fix

--- src/config.ts
+++ src/config.ts
@@ -290,7 +290,7 @@ export const siteConfig: SiteConfig = {
   // Profile Picture
   profilePicture: {
     // [CONFIG:PROFILE_PICTURE_ENABLED]
-    enabled: true, 
+    enabled: false, 
     // [CONFIG:PROFILE_PICTURE_IMAGE]
     image: "/favicon.png", // Path to your profile image (place in public/ directory)
     // [CONFIG:PROFILE_PICTURE_ALT]
